fdb92587406ac88aa31a92d86bce2cf222eb787b
avex <149749883+avexxx3@users.noreply.github.com>
2023-12-02 00:08:55 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "Functions.h"
 #include "Words.h"
 #include <iostream>
 #include <string>
@@ -6,12 +8,12 @@
 void playGame()
 {
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
-    sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height), "HANGMAN", sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height), "FraMan", sf::Style::Fullscreen);
     sf::Texture texture; sf::Sprite sprite; sf::Text data; sf::Text categ; sf::Text word; sf::Font font; sf::Event event; // Create all objects
     // word --> dashes being updated live as user inputs
     // data --> hangman text which then changes to turns left
     int j = 0, count = 0; //count is the number of correct alphabets
-    bool end = 0, idk = 1; //idk = 1 means continue
+    bool end = 0; //idk = 1 means continue
     std::string category;
     std::string execution = Word(category); //execution contains the actual word
     execution[0] = tolower(execution[0]);
@@ -37,80 +39,32 @@ void playGame()
     {
         while (window.pollEvent(event))
         {
-            // exitProg(i, event, window);
+            exitProg(j, event, window);
 
-
-            //if (event.type == event.KeyReleased && keyPress(event)) //keypress returns the key pressed as a character
-            //    idk = 1;
-
-            if (event.type == event.KeyReleased && keyPress(event) != '.') {
-                //check input
-                char input = keyPress(event);
-                int temp = count;
-                std::cout << input;
-                for (int i = 0; i < guess.size(); i++) {
-                    if (input == execution[i]) {
-                        std::cout << "Correct.\n";
-                        guess[i] = execution[i];
-                        execution[i] = '*';
-                        count++;
-                    }
-                }
-
-                if (count == execution.length()) {
-                    end = 1;
-                }
-
-                if (temp == count) {
-                    j++;
-                }
-                //set data
-                if (event.type == sf::Event::KeyReleased && j < 8) {
-                    std::string turns, num, path;
-                    num = std::to_string(j);
-                    path = "Hangman\\x.png";
-                    path[41] = num[0];
-
-                    if (7 - j != 0) {
-                        num = std::to_string(7 - j);
-                        turns = "Turns left: x";
-                        turns[12] = num[0];
-                    }
-
-                    else
-                        turns = "Game over!";
-
-                    texture.loadFromFile(path);
-                    data.setString(turns);
-                }
+            if (event.type == event.KeyReleased && keyPress(event) != '.' && !end) {
+                checkInput(j, guess, execution, event, count, end);
+                setData(j, texture, data, event);
                 word.setString(guess);
             }
-
-            //displayData(window, sprite, data, word, categ, i, end, idk);
-
-            window.clear();
-            window.draw(sprite);
-            window.draw(data);
             if (end)
-                data.setString("Congratulations!");
-            if (idk) {
-                window.draw(categ);
-                window.draw(word);
-            }
-            window.display();
+                data.setString("Congratulations!\n\n\n\n\n\n\n\nPress ESC to return to main menu!");
 
-            if (end || j>=8) {
+            displayData(window, sprite, data, word, categ, j, end);
+
+            if (end && sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
                 window.close();
-            }
         }
     }
 }
 
-int main()
-{
+int main() {
+    sf::Music music;
+    music.openFromFile("Stellar Stellar.mp3");
+    music.play();
+
     sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 
-    sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height), "HANGMAN", sf::Style::Fullscreen);
+    sf::RenderWindow window(sf::VideoMode(desktop.width, desktop.height), "FraMan", sf::Style::Fullscreen);
 
     sf::Texture bg;
     bg.loadFromFile("Hangman\\Base.png");
@@ -128,19 +82,15 @@ int main()
     sf::Texture play;
     play.loadFromFile("Hangman\\Play.png");
     sf::Sprite playSprite(play);
-    playSprite.scale(scale, scale);
     sf::Texture instructions;
     instructions.loadFromFile("Hangman\\Instructions.png");
     sf::Sprite instructionsSprite(instructions);
-    instructionsSprite.scale(scale, scale);
     sf::Texture exit;
     exit.loadFromFile("Hangman\\Exit.png");
     sf::Sprite exitSprite(exit);
-    exitSprite.scale(scale, scale);
     sf::Texture instr;
     instr.loadFromFile("Hangman\\7.png");
     sf::Sprite instrSprite(instr);
-    instrSprite.scale(scale, scale);
     sf::Texture rtrn;
     rtrn.loadFromFile("Hangman\\Play.png");
     sf::Sprite rtrnSprite(rtrn);
@@ -167,7 +117,7 @@ int main()
 
     int i = 0;
     bool showInstr=false;
-    bool showButtons=true;
+    bool showButtons = true;
     while (window.isOpen())
     {
         sf::Event event;
